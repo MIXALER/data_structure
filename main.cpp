a0d98c985db5715145f73fa569e87e88952567de
MIXALER <yuanhangchn@outlook.com>
2021-04-01 21:02:04 +0800

    add linklist

--- main.cpp
+++ main.cpp
@@ -1,10 +1,14 @@
 #include <iostream>
-#include "solution2.h"
+#include "link_list.h"
 
 int main()
 {
-    Solution mysolution;
-    cout<<mysolution.isValid_2("()")<<endl;
+    LinkList link_list;
+    vector<int> list_elements{1, 2, 3, 4, 5};
+    ListNode* lists = link_list.CreatListEndInsert(list_elements);
+    link_list.PrintListNode(lists);
+    ListNode* reverse_lists = link_list.ReverseListRecursion(lists);
+    link_list.PrintListNode(reverse_lists);
     return 0;
 }
 
